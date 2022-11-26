3c2cea6188bad6a497c0f3d61cb1058d66f879e9
Saravanakumar JN <saravanakumar.jn@coinswitch.co>
2022-11-26 15:31:29 +0530

    basic fund transfer contract - live test net

--- basic-fundTransfer-liveTestNetwork/scripts/deployFundTransfer.js
+++ basic-fundTransfer-liveTestNetwork/scripts/deployFundTransfer.js
@@ -1,23 +1,23 @@
 const hre = require("hardhat")
 
 const runScript = async () => {
-  const [deployer, receiver] = await hre.ethers.getSigners()
-
   const FundTransferContract = await hre.ethers.getContractFactory('FundTransfer')
   const FundTransferContractInstance = await FundTransferContract.deploy()
   await FundTransferContractInstance.deployed()
 
-  // add fund to contract
-  await FundTransferContractInstance.receiveEther({value: 2})
+  console.log("Contract address", FundTransferContractInstance.address)
 
-  const receiverBalance1 = await hre.ethers.provider.getBalance(receiver.address)
+  // add fund to contract from deployer's wallet
+  await FundTransferContractInstance.receiveEther({value: 20000000000})
+
+  const receiverBalance1 = await hre.ethers.provider.getBalance("0x57a7134E58c279506282ab9211F6d9C14ab28AB1")
   
-  const tx = await FundTransferContractInstance.transferEther(receiver.address, 1)
+  const tx = await FundTransferContractInstance.transferEther("0x57a7134E58c279506282ab9211F6d9C14ab28AB1", 10000000000)
   // get the event emitted
   const txReceipt = await tx.wait()
   console.log(txReceipt.events.filter((x) => {return x.event == "ETransferEther"}))
 
-  const receiverBalance2 = await hre.ethers.provider.getBalance(receiver.address)
+  const receiverBalance2 = await hre.ethers.provider.getBalance("0x57a7134E58c279506282ab9211F6d9C14ab28AB1")
   console.log(receiverBalance1, receiverBalance2)
 }
 
